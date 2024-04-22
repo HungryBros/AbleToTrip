7def77298d62984ef1a8aefcbea7a84dbbe70fc4
이수민 <suminjeff@gmail.com>
2024-04-22 17:57:38 +0900

    [BE] feat: 관광지 API 초안

--- backend/abletotrip/settings/base.py
+++ backend/abletotrip/settings/base.py
@@ -20,12 +20,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'attraction',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
