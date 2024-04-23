acd0f414f6e6c938872287467577ce5d763ad8ae
이수민 <suminjeff@gmail.com>
2024-04-23 17:26:20 +0900

    [BE] feat: User view 초안 작성

---
+++ backend/member/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'address']  # 주소 필드 추가
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            address=validated_data.get('address')  # 주소 정보 저장
+        )
+        return user
