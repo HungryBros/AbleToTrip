9a3bb5d2898c50cbb500605c2713e12ea8a292f4
이운재 <hil00077@khu.ac.kr>
2024-04-24 16:25:36 +0900

    [BE] feat: docker 세팅 완료

--- backend/abletotrip/settings/base.py
+++ backend/abletotrip/settings/base.py
@@ -11,9 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+scxk&u_3(jeeu^j(ql)$m30o*%l9$6&nnmmo@!+4b!p1adof='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['k10a607.p.ssafy.io', 'www.k10a607.p.ssafy.io']
+
 
 
 # Application definition
@@ -93,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -106,6 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/app/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
