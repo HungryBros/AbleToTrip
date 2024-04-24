478dec9678e628e4c29dde548c8069034e1814f1
정진영 <wlsehfdl95@naver.com>
2024-04-24 17:23:05 +0900

    [BE] feat: 관광지 이미지 업데이트 admin 전용 api

--- backend/attraction/urls.py
+++ backend/attraction/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path
 from . import views
 
-app_name = ''
+app_name = ""
 urlpatterns = [
     # 홈 - 기본
-    path('', views.attraction, name='attraction'),
-
+    path("", views.attraction, name="attraction"),
     # 홈 - 카테고리2 직접 선택
-    path('specific/', views.attraction_specific, name='attraction_specific'),  # 특정 관광지 정보 요청
-
+    path(
+        "specific/", views.attraction_specific, name="attraction_specific"
+    ),  # 특정 관광지 정보 요청
     # 홈 - 카테고리1 더보기
-    path('more/<str:category1>/', views.attraction_more, name='attraction_more'),
-
+    path("more/<str:category1>/", views.attraction_more, name="attraction_more"),
     # 관광지 검색
-    path('search/', views.attraction_search, name='attraction_search'),
+    path("search/", views.attraction_search, name="attraction_search"),
+    # 이미지 저장 - admin용
+    path(
+        "update-attraction-images/",
+        views.update_attraction_images,
+        name="update_attraction_images",
+    ),
 ]
--- backend/attraction/views.py
+++ backend/attraction/views.py
@@ -1,24 +1,56 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Attraction
+from .serializers import AttractionSerializer
+import os
 
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(["GET"])
 def attraction(request):
     return Response({"response": "attraction"})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def attraction_specific(request):
     return Response({"response": "attraction_specific"})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def attraction_more(request, category1):
     return Response({"response": f"{category1}"})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def attraction_search(request):
     return Response({"response": "attraction_search"})
+
+
+@api_view(["PUT"])
+def update_attraction_images(request):
+    # 모든 Attraction 데이터 불러오기
+    attractions = Attraction.objects.all()
+
+    # 각 Attraction 레코드마다 이미지 데이터 업데이트
+    for attraction in attractions:
+        # 이미지 경로
+        image_path = f"attraction/images/{attraction.attraction_name}.png"
+
+        # 이미지 파일 존재 여부 확인
+        if os.path.exists(image_path):
+            try:
+                # 이미지 로드 및 데이터 업데이트
+                with open(image_path, "rb") as image_file:
+                    image_data = image_file.read()
+                    attraction.attraction_image = image_data
+                    attraction.save()
+                    print(f"{attraction.attraction_name} 이미지 업로드 성공")
+
+            except Exception as e:
+                print(f"{attraction.attraction_name} 이미지 업로드 실패: {e}")
+
+        else:
+            print(f"{attraction.attraction_name} 이미지 파일 없음")
+
+    return Response({"message": "작업 완료"})
