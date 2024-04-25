7f9858202c8eb16c8a69ba6e82a47c54b3f12b60
이운재 <hil00077@khu.ac.kr>
2024-04-25 14:10:22 +0900

    [BE] chore: 호스트에 로컬주소 추가

--- backend/abletotrip/settings/base.py
+++ backend/abletotrip/settings/base.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['k10a607.p.ssafy.io', 'www.k10a607.p.ssafy.io']
+ALLOWED_HOSTS = ['k10a607.p.ssafy.io', 'www.k10a607.p.ssafy.io', '127.0.0.1']
 
 
 
