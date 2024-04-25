fc8d7c551197454c00655f3542eb397ecdd1990b
이수민 <suminjeff@gmail.com>
2024-04-25 11:19:49 +0900

    [BE] feat: Member API 수정

--- backend/member/urls.py
+++ backend/member/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('post_address/', views.post_address, name='post_address'),
     path('signin/', views.signin, name='signin'),
+    path('info/', views.info, name='info'),
 ]
