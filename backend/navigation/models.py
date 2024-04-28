537a71a9b235a09745813525b877272a861f32a4
정진영 <wlsehfdl95@naver.com>
2024-04-28 18:50:28 +0900

    [BE] feat: 지하철 관련 model 작성 및 migrate 완료

--- backend/navigation/models.py
+++ backend/navigation/models.py
@@ -5,24 +5,22 @@ from django.db import models
 class Station(models.Model):
     station_fullname = models.CharField(primary_key=True, max_length=30)
     station_name = models.CharField(max_length=20)
+    station_code = models.CharField(max_length=10, null=False)
+    line_number = models.SmallIntegerField()
+
+
+class Convenient(models.Model):
+    station_fullname = models.ForeignKey(Station, on_delete=models.PROTECT)
+    station_name = models.CharField(max_length=20)
     line_number = models.SmallIntegerField()
     line_name = models.CharField(max_length=10)
     direction1 = models.CharField(max_length=50)
     direction2 = models.CharField(max_length=50)
-    elevator_subtitle = models.TextField(null=True)
+    elevator_location = models.TextField(null=True)
     direction1_first_train = models.CharField(max_length=50, null=True)
     direction1_last_train = models.CharField(max_length=50, null=True)
     direction2_first_train = models.CharField(max_length=50, null=True)
     direction2_last_train = models.CharField(max_length=50, null=True)
-
-
-class Elevator(models.Model):
-    line_number = models.SmallIntegerField()
-    station_name = models.CharField(max_length=20)
-    elevator_number = models.CharField(unique=True, max_length=10)
-    elevator_location = models.CharField(max_length=100, null=True)
-    operation_section = models.CharField(max_length=10, null=True)
-    station_fullname = models.ForeignKey(Station, on_delete=models.PROTECT)
 
 
 class Ramp(models.Model):
@@ -36,7 +34,7 @@ class Lift(models.Model):
     line_number = models.SmallIntegerField()
     station_name = models.CharField(max_length=20)
     lift_location = models.CharField(max_length=255, null=True)
-    operation_section = models.CharField(max_length=10, null=True)
+    operation_section = models.CharField(max_length=30, null=True)
     station_fullname = models.ForeignKey(Station, on_delete=models.PROTECT)
 
 
