240410987eeb1dcd30ac1bf6e16fe8ddae5a7047
chan ui song <dforce103@gmail.com>
2024-04-30 14:55:22 +0900

    [FE]feat: fe/loginCSS

--- frontend/abletotrip/app/src/main/java/com/hungrybrothers/abletotrip/ui/screen/LoginScreen.kt
+++ frontend/abletotrip/app/src/main/java/com/hungrybrothers/abletotrip/ui/screen/LoginScreen.kt
@@ -1,50 +1,101 @@
 package com.hungrybrothers.abletotrip.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.hungrybrothers.abletotrip.KakaoAuthViewModel
+import com.hungrybrothers.abletotrip.R
 import com.hungrybrothers.abletotrip.ui.navigation.NavRoute
 
 @Composable
 fun LoginScreen(navController: NavController) {
     val viewModel: KakaoAuthViewModel = viewModel()
+    val loggedIn = viewModel.loggedIn.observeAsState().value
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(20.dp),
-    ) {
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = {
-            viewModel.handleKakaoLogin()
-        }) {
-            Text(text = "카카오 로그인하기")
-        }
-        // 만약 로그인 상태가 true라면 네비게이션을 수행합니다.
-        // 로그인 상태가 true라면 네비게이션을 수행합니다.
-        val loggedIn = viewModel.loggedIn.observeAsState().value
-        if (loggedIn == true) {
-            LaunchedEffect(loggedIn) { // 여기에서 key1을 loggedIn으로 변경합니다.
-                navController.navigate(NavRoute.ADDRESS.routeName) {
-                    // 기존에 쌓인 화면들을 클리어하고 새 화면을 탑 스택에 올립니다.
-                    popUpTo(navController.graph.startDestinationId)
-                    launchSingleTop = true
-                }
+    if (loggedIn == true) {
+        LaunchedEffect(loggedIn) {
+            navController.navigate(NavRoute.ADDRESS.routeName) {
+                popUpTo(navController.graph.startDestinationId)
+                launchSingleTop = true
             }
         }
+    }
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(bottom = 16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.weight(1f),
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.easytriplogo),
+                    contentDescription = "EasyTrip Logo",
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 40.dp),
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.wheelchair),
+                    contentDescription = "Accessibility Symbol",
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+
+            ImageAsButton(onClick = { viewModel.handleKakaoLogin() })
+        }
+    }
+}
+
+@Composable
+fun ImageAsButton(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .height(48.dp)
+            .clickable(onClick = onClick)
+            .clip(RoundedCornerShape(8.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.kakao_login_medium_wide),
+            contentDescription = "Kakao Login Button",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+
+        )
     }
 }
 
