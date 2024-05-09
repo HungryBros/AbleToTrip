f3e65f47c1843bb618dbcd63b9c88888bcbb356f
chan ui song <dforce103@gmail.com>
2024-05-09 12:38:24 +0900

    [FE feat: fe/loginrefactor

--- frontend/abletotrip/app/build.gradle.kts
+++ frontend/abletotrip/app/build.gradle.kts
@@ -89,6 +89,8 @@ dependencies {
     implementation("com.kakao.sdk:v2-friend:2.20.1") // 피커 API 모듈
     implementation("com.kakao.sdk:v2-navi:2.20.1") // 카카오내비 API 모듈
     implementation("com.kakao.sdk:v2-cert:2.20.1") // 카카오톡 인증 서비스 API 모듈
+    // security저장소
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
 
     // 네비게이션
     implementation("androidx.navigation:navigation-compose:2.7.7")
