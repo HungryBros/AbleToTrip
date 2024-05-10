bc7a1a382905ac911bbc1435711cf678f317d190
chan ui song <dforce103@gmail.com>
2024-05-10 17:02:16 +0900

    [FE] feat: fe/detailcss

--- frontend/abletotrip/app/src/main/java/com/hungrybrothers/abletotrip/ui/screen/DetailScreen.kt
+++ frontend/abletotrip/app/src/main/java/com/hungrybrothers/abletotrip/ui/screen/DetailScreen.kt
@@ -1,20 +1,24 @@
 package com.hungrybrothers.abletotrip.ui.screen
+
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -26,12 +30,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
+import com.hungrybrothers.abletotrip.R
+import com.hungrybrothers.abletotrip.ui.components.HeaderBar
 import com.hungrybrothers.abletotrip.ui.datatype.AttractionDetail
-import com.hungrybrothers.abletotrip.ui.navigation.NavRoute
 import com.hungrybrothers.abletotrip.ui.network.AttractionDetailRepository
 import com.hungrybrothers.abletotrip.ui.viewmodel.AttractionDetailViewModel
 
@@ -39,7 +52,6 @@ import com.hungrybrothers.abletotrip.ui.viewmodel.AttractionDetailViewModel
 fun DetailScreen(
     navController: NavController,
     itemId: Int?,
-//    attractionDetailViewModel: AttractionDetailViewModel = viewModel(),
 ) {
     val attractionDetailViewModel: AttractionDetailViewModel =
         remember {
@@ -56,96 +68,283 @@ fun DetailScreen(
     }
 
     val attractionDetail by attractionDetailViewModel.attractionDetailData.collectAsState()
-//    Column {
-//        Text(text = "$attractionDetail")
-//    }
-//
-    attractionDetail?.let { detail ->
+    Box(
+        modifier =
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp),
+    ) {
+        HeaderBar(navController = navController, showBackButton = true)
+        Spacer(modifier = Modifier.size(16.dp))
         Column(
             modifier =
-                Modifier
-                    .verticalScroll(rememberScrollState())
-                    .padding(16.dp),
+            Modifier
+                .padding(top = 72.dp, bottom = 72.dp) // Adjust the padding as needed
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Top,
         ) {
-            Image(
-                painter = rememberAsyncImagePainter(model = detail.image_url), // 이미지 없음
-                contentDescription = "Attraction Image",
-                modifier =
+            attractionDetail?.let { detail ->
+                Image(
+                    painter = rememberAsyncImagePainter(model = detail.image_url),
+                    contentDescription = "Attraction Image",
+                    modifier =
                     Modifier
                         .fillMaxWidth()
-                        .height(240.dp),
-                contentScale = ContentScale.Crop,
-            )
-            Spacer(Modifier.height(16.dp))
-            Text(text = detail.attraction_name, style = MaterialTheme.typography.headlineMedium)
-            if (detail.attraction_sub_name != null) {
-                Text(text = detail.attraction_sub_name, style = MaterialTheme.typography.bodyMedium)
-            }
-            Text(text = "${detail.si} ${detail.gu}, ${detail.dong}", style = MaterialTheme.typography.bodySmall)
-            Text(
-                text = "주소: ${detail.road_name_address ?: detail.lot_number_address}",
-                style = MaterialTheme.typography.bodySmall,
-            )
-            Text(text = "연락처: ${detail.contact_number ?: "정보 없음"}", style = MaterialTheme.typography.bodySmall)
-            Text(text = "운영시간: ${detail.operation_hours}", style = MaterialTheme.typography.bodySmall)
-            Text(text = "홈페이지: ${detail.homepage_url}", style = MaterialTheme.typography.bodySmall)
-
-            Spacer(Modifier.height(24.dp))
-            FacilitiesGrid(detail)
-            RouteButton(navController, detail.latitude, detail.longitude, detail.lot_number_address)
+                        .height(240.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    contentScale = ContentScale.Crop,
+                )
+                Spacer(Modifier.height(16.dp))
+                Row(modifier = Modifier, verticalAlignment = Alignment.Bottom) {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                        text = detail.attraction_name,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Text(
+                        text = detail.category2,
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Gray,
+                    )
+                }
+                InfoRow(R.drawable.locationon, detail.road_name_address ?: detail.lot_number_address)
+                InfoRow(
+                    R.drawable.clock,
+                    if (detail.operation_hours.isNotEmpty()) "OPEN . ${detail.operation_hours}" else "OPEN . 정보 없음",
+                )
+                InfoRow(
+                    R.drawable.calendarmonth,
+                    if (detail.closed_days.isNotEmpty()) "휴무일 . ${detail.closed_days}" else "휴무일 . 정보 없음",
+                )
+                InfoLinkRow(R.drawable.call, detail.contact_number, "tel:")
+                InfoLinkRow(R.drawable.earth, detail.homepage_url, "")
+                InfoRow(R.drawable.ticket2, if (detail.is_entrance_fee) "입장료 . 무료" else "입장료 . 유료")
+                Spacer(Modifier.height(24.dp))
+                FacilitiesGrid(detail)
+            } ?: Text("Loading details...", style = MaterialTheme.typography.bodyLarge)
         }
-    } ?: Text("Loading details...", style = MaterialTheme.typography.bodyLarge)
+
+        // Fixed RouteButton at the bottom
+        attractionDetail?.let { detail ->
+            RouteButton(
+                navController = navController,
+                latitude = detail.latitude,
+                longitude = detail.longitude,
+                lotNumberAddress = detail.lot_number_address,
+                modifier =
+                Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(vertical = 16.dp),
+            )
+        }
+    }
+}
+
+@Composable
+fun RouteButton(
+    navController: NavController,
+    latitude: Double,
+    longitude: Double,
+    lotNumberAddress: String,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = {
+            navController.navigate("DEPARTURE/${latitude.toFloat()}/${longitude.toFloat()}/$lotNumberAddress")
+        },
+        modifier =
+        modifier
+            .fillMaxWidth()
+            .height(48.dp),
+    ) {
+        Text("길찾기", style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+fun InfoRow(
+    iconId: Int,
+    text: String,
+) {
+    Row(
+        modifier = Modifier.padding(top = 16.dp),
+    ) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = "icon",
+            modifier =
+            Modifier
+                .padding(horizontal = 8.dp)
+                .size(16.dp)
+                .align(Alignment.CenterVertically),
+            tint = Color.Unspecified,
+        )
+        Text(text = text, style = MaterialTheme.typography.bodySmall)
+    }
+}
+
+@Composable
+fun InfoLinkRow(
+    iconId: Int,
+    text: String?,
+    schemePrefix: String = "",
+) {
+    val context = LocalContext.current
+    val annotatedLink =
+        buildAnnotatedString {
+            if (!text.isNullOrEmpty()) {
+                val link = "$schemePrefix$text"
+                append(text)
+                addStringAnnotation("URL", link, 0, text.length)
+            } else {
+                append("정보 없음")
+            }
+        }
+
+    Row(
+        modifier = Modifier.padding(top = 16.dp),
+    ) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = "icon",
+            modifier =
+            Modifier
+                .padding(horizontal = 8.dp)
+                .size(16.dp)
+                .align(Alignment.CenterVertically),
+            tint = Color.Unspecified,
+        )
+        ClickableText(
+            text = annotatedLink,
+            style = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.primary),
+            onClick = { offset ->
+                annotatedLink.getStringAnnotations("URL", offset, offset).firstOrNull()?.let {
+                    openUrl(context, it.item)
+                }
+            },
+        )
+    }
+}
+
+fun openUrl(
+    context: android.content.Context,
+    url: String,
+) {
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    context.startActivity(intent)
 }
 
 @Composable
 fun FacilitiesGrid(attractionDetail: AttractionDetail) {
     Column(modifier = Modifier.padding(top = 16.dp)) {
-        Text("시설 정보", style = MaterialTheme.typography.titleMedium)
+        Text("시설 정보", style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.Bold)
         Spacer(Modifier.height(8.dp))
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            FacilityIcon(attractionDetail.is_free_parking, "무료 주차")
-            FacilityIcon(attractionDetail.is_paid_parking, "유료 주차")
-            FacilityIcon(attractionDetail.is_entrance_fee, "입장료")
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            FacilityItem(
+                isAvailable = attractionDetail.is_free_parking,
+                iconId = R.drawable.p,
+                availableDescription = "무료 주차",
+            )
+            FacilityItem(
+                isAvailable = attractionDetail.is_paid_parking,
+                iconId = R.drawable.p,
+                availableDescription = "유료 주차",
+            )
+            FacilityItem(
+                isAvailable = attractionDetail.is_audio_guide,
+                iconId = R.drawable.audio,
+                availableDescription = "오디오 가이드",
+            )
         }
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            FacilityIcon(attractionDetail.is_disabled_restroom, "장애인 화장실")
-            FacilityIcon(attractionDetail.is_disabled_parking, "장애인 주차")
-            FacilityIcon(attractionDetail.is_large_parking, "대형 주차장")
-        }
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            FacilityIcon(attractionDetail.is_audio_guide, "오디오 가이드")
+
+        Spacer(Modifier.height(8.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            FacilityItem(
+                isAvailable = attractionDetail.is_disabled_restroom,
+                iconId = R.drawable.men,
+                availableDescription = "장애인 화장실",
+            )
+            FacilityItem(
+                isAvailable = attractionDetail.is_large_parking,
+                iconId = R.drawable.parking,
+                availableDescription = "대형차 주차",
+            )
+            FacilityItem(
+                isAvailable = attractionDetail.is_disabled_parking,
+                iconId = R.drawable.p,
+                availableDescription = "장애인용 주차장",
+            )
         }
     }
+    Spacer(modifier = Modifier.size(80.dp))
 }
 
 @Composable
-fun FacilityIcon(
+fun FacilityItem(
     isAvailable: Boolean,
-    label: String,
+    iconId: Int,
+    availableDescription: String,
 ) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Icon(
-            imageVector = if (isAvailable) Icons.Filled.Check else Icons.Filled.Close,
-            contentDescription = if (isAvailable) "$label 가능" else "$label 불가능",
-            tint = if (isAvailable) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.error,
-            modifier = Modifier.size(24.dp),
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(8.dp),
+    ) {
+        Box(
+            modifier = Modifier.size(32.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            Icon(
+                painter = painterResource(id = iconId),
+                contentDescription = availableDescription,
+                modifier = Modifier.size(24.dp),
+                tint = Color.Unspecified,
+            )
+            if (!isAvailable) {
+                DrawXMark()
+            }
+        }
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = availableDescription,
+            style = MaterialTheme.typography.bodySmall,
+            color = if (isAvailable) Color.Black else Color.Gray,
         )
-        Text(text = label, style = MaterialTheme.typography.bodySmall)
     }
 }
 
 @Composable
-fun RouteButton(navController: NavController, latitude: Double, longitude: Double, lotNumberAddress: String) {
-    Button(
-        onClick = {
-            navController.navigate("DEPARTURE/${latitude.toFloat()}/${longitude.toFloat()}/$lotNumberAddress")
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .height(48.dp)
-    ) {
-        Text("Get Directions")
+fun DrawXMark() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        drawXMark(this)
     }
 }
 
+private fun drawXMark(scope: DrawScope) {
+    val size = scope.size.minDimension
+    val strokeWidth = size / 8
+    val path =
+        Path().apply {
+            moveTo(0f, 0f)
+            lineTo(size, size)
+            moveTo(size, 0f)
+            lineTo(0f, size)
+        }
+
+    scope.drawPath(
+        path = path,
+        color = Color.Red,
+        style = androidx.compose.ui.graphics.drawscope.Stroke(width = strokeWidth),
+    )
+}
