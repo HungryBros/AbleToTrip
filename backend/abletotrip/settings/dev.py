64df97ec129c793e94ac1fd9a105118501a8c550
이수민 <suminjeff@gmail.com>
2024-05-13 13:08:07 +0900

    [BE] chore: dev.py랑 gitignore 원상복구

--- backend/abletotrip/settings/dev.py
+++ backend/abletotrip/settings/dev.py
@@ -2,14 +2,3 @@ print("RUNNING ON DEVELOPMENT ENVIRONMENT")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "0000",
-        "HOST": "localhost",  # 또는 데이터베이스가 호스팅되는 IP 주소
-        "PORT": 5432,  # 기본 PostgreSQL 포트
-    }
-}
