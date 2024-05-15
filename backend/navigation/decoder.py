618c2faa1f1e2c17b5da1669d55f4f51e1518e27
이수민 <suminjeff@gmail.com>
2024-05-15 21:04:40 +0900

    [BE] fix: 디코더 코드 수정

--- backend/navigation/decoder.py
+++ backend/navigation/decoder.py
@@ -2,48 +2,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from member.utils import is_logged_in
 from rest_framework.decorators import api_view
+import polyline
 
 
-def replace_backslashes(input_string):
-    """Replaces all single backslashes with double backslashes in a given string.
-
-    Args:
-        input_string (str): The string to modify.
-
-    Returns:
-        str: Modified string where all backslashes are doubled.
-    """
-    return input_string.replace("\\", "\\\\")
-
-
-def decode_polyline(polyline_str):
-    """Decodes a polyline that was encoded using the Google Maps algorithm.
-
-    Args:
-        polyline_str (str): Encoded polyline string.
-
-    Returns:
-        List of tuples where each tuple represents latitude and longitude.
-    """
-    index, lat, lng, coordinates = 0, 0, 0, []
-    while index < len(polyline_str):
-        for is_lat in [True, False]:  # True for latitude, False for longitude
-            result, shift = 0, 0
-            byte = 0x20
-            while byte >= 0x20:
-                byte = ord(polyline_str[index]) - 63
-                index += 1
-                result |= (byte & 0x1F) << shift
-                shift += 5
-            if result & 1:
-                result = ~result
-            result >>= 1
-            coord = result / 1e5
-            if is_lat:  # This is for latitude
-                lat += coord
-            else:  # This is for longitude
-                lng += coord
-                coordinates.append([lat, lng])
+def decode_polyline(encoded_polyline):
+    decoded = polyline.decode(encoded_polyline)
+    coordinates = []
+    for d in decoded:
+        coordinates.append([d[0], d[1]])
     return coordinates
 
 
@@ -56,8 +22,7 @@ def get_decoded_polyline(request):
         )
     try:
         encoded_polyline = request.data.get("input")
-        non_escape_encoded_polyline = replace_backslashes(encoded_polyline)
-        decoded_polyline = decode_polyline(non_escape_encoded_polyline)
+        decoded_polyline = decode_polyline(encoded_polyline)
         is_success = 1 if decoded_polyline else 0
         data = {
             "success": is_success,
@@ -68,7 +33,8 @@ def get_decoded_polyline(request):
             status=status.HTTP_200_OK if is_success else status.HTTP_204_NO_CONTENT,
         )
     except:
-        return Response(
-            {"message": "잘못된 요청입니다."},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+        data = {
+            "success": 0,
+            "data": [[37.501286, 127.0396029]],
+        }
+        return Response(data, status=status.HTTP_200_OK)
